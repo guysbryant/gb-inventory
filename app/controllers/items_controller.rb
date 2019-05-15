0ac504c82a23ce06c7defaddf9f67f5e104c347c
guysbryant <guy.s.bryant@gmail.com>
2019-05-15 17:16:13 -0400

    Stub out controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController 
+    #add item to inventory
+
+    #view all items in inventory
+
+    #update an item 
+
+    #delete an item
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,30 @@
 class SessionsController < ApplicationController
+    #login
+    get "/login" do 
+        erb :"login"
+    end
+
+    post "/login" do 
+        #find the user in activerecord
+
+        #authenticate user
+
+        #-add to session and redirect || redirect to login again
+    end
+
+    #signup
+    get "/signup" do 
+        erb :"signup"
+    end
+
+    post "/signup" do 
+        #create user
+
+        #add user to session and redirect or redirect to signup with flash message
+    end
+
+    #logout
+    get "/logout" do 
+        #clear session
+    end
 end
