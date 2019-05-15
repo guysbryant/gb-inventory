a3cb2e99d0e59352b4fcbfff8c176ee5822ee4e9
guysbryant <guy.s.bryant@gmail.com>
2019-05-15 17:51:50 -0400

    Add route to inventory show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,11 @@
 class ItemsController < ApplicationController 
     #add item to inventory
 
-    #view all items in inventory
+    #view all items in user's inventory
+    get "/inventory/show/:id" do 
+        @user = User.find(params[:id])
+        erb :"/items/show"
+    end
 
     #update an item 
 
