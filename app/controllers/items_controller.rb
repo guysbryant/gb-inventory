07f925b4b0495e1cb1abdc66c3ffda9c9bff7778
guysbryant <guy.s.bryant@gmail.com>
2019-05-15 18:53:31 -0400

    Add new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,21 @@
 class ItemsController < ApplicationController 
     #add item to inventory
+    get "/items/new" do 
+        erb :"/items/new"
+    end
+
+    post "/items/new" do 
+        #create item
+        @item = Item.new(params)
+
+        if @item.save
+            redirect "/inventory/show/#{current_user.id}"
+        else
+            flash[:error] = "Couldn't add item: #{@item.errors.full_messages.to_sentence}"
+            redirect "/items/new"
+        end
+
+    end
 
     #view all items in user's inventory
     get "/inventory/show/:id" do 
