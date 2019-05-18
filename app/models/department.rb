6d50a05d4a976c868673e9950f78eeb95347d6a4
guysbryant <guy.s.bryant@gmail.com>
2019-05-18 14:31:21 -0400

    Add name validation to department model

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,4 +2,7 @@ class Department < ActiveRecord::Base
     has_many :items
     has_many :users, through: :user_departments
     has_many :user_departments
+
+    validates :name, presence: true 
+    validates :name, uniqueness: true
 end
