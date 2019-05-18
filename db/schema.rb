43beac4c09534aa6431df6eb63949900b12cf125
guysbryant <guy.s.bryant@gmail.com>
2019-05-18 08:24:14 -0400

    DB changes not important

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190517001118) do
+ActiveRecord::Schema.define(version: 20190518121559) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20190517001118) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "quantity"
+    t.string   "details"
   end
 
   create_table "users", force: :cascade do |t|
